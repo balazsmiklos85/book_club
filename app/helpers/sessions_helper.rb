c9363cd0c4a78bb46cb1104adae84390b5a775ee
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-04-02 08:51:42 +0200

    SessionsHelper simplified so the User model is not accessed from views.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,19 +3,14 @@
 # Sessions helper module. Contains methods for the session handling of the user.
 module SessionsHelper
   def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    session[:user] = user
   end
 
   def logged_in?
-    !current_user.nil?
+    !session[:user].nil?
   end
 
   def log_out
-    session.delete(:user_id)
-    @current_user = nil
+    session.delete(:user)
   end
 end
