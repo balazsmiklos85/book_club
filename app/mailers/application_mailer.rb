47e13c580583d42710f47b9f89f6f20400422f08
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-08-15 15:29:23 +0000

    Spring Boot initialized

--- app/mailers/application_mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Base class for all mailers. This is an abstract class, and should not be instantiated.
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
