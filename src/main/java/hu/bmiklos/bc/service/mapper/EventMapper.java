6e887acee81c5db0086d22b51bbb7340015868b0
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-09-11 20:48:59 +0000

    participants and host may have historical users who are not registered

--- src/main/java/hu/bmiklos/bc/service/mapper/EventMapper.java
+++ src/main/java/hu/bmiklos/bc/service/mapper/EventMapper.java
@@ -3,8 +3,11 @@ package hu.bmiklos.bc.service.mapper;
 import java.time.Instant;
 import java.util.List;
 
+import org.springframework.lang.Nullable;
+
 import hu.bmiklos.bc.model.Event;
 import hu.bmiklos.bc.model.Participant;
+import hu.bmiklos.bc.model.User;
 import hu.bmiklos.bc.service.dto.BookDto;
 import hu.bmiklos.bc.service.dto.CreateEventDto;
 import hu.bmiklos.bc.service.dto.GetEventDto;
@@ -12,7 +15,8 @@ import hu.bmiklos.bc.service.dto.UserDto;
 
 public class EventMapper {
 
-    private EventMapper() {}
+    private EventMapper() {
+    }
 
     public static CreateEventDto mapToDto(Event lastEvent) {
         return new CreateEventDto(lastEvent.getBookId(), lastEvent.getTime(), lastEvent.getHostId());
@@ -21,18 +25,25 @@ public class EventMapper {
     public static GetEventDto mapToGetDto(Event event) {
         Instant time = event.getTime();
         BookDto book = BookMapper.mapToDto(event.getBook());
-        UserDto host = null;
-        if (event.getHostByHostId() != null) {
-            host = UserMapper.mapToDto(event.getHostByHostId());
-        }
-        if (event.getHostByHostExternalId() != null) {
-            host = UserMapper.mapToDto(event.getHostByHostExternalId());
-        }        
+        UserDto host = mapHostByIdOrExternalId(event);
         List<UserDto> participants = event.getParticipants()
-            .stream()
-            .map(Participant::getUser)
-            .map(UserMapper::mapToDto)
-            .toList();
+                .stream()
+                .map(participant -> UserMapper.mapToDto(participant.getUser(), participant.getParticipantExternalId()))
+                .toList();
         return new GetEventDto(time, book, host, event.getHostExternalId(), participants);
+    }
+
+    @Nullable
+    private static UserDto mapHostByIdOrExternalId(Event event) {
+        UserDto host = null;
+        User hostByHostId = event.getHostByHostId();
+        if (hostByHostId != null) {
+            host = UserMapper.mapToDto(hostByHostId);
+        }
+        User hostByHostExternalId = event.getHostByHostExternalId();
+        if (hostByHostExternalId != null) {
+            host = UserMapper.mapToDto(hostByHostExternalId, event.getHostExternalId());
+        }
+        return host;
     }
 }
