5d7aaf4058f5c2ed77e0710738094feafbecc474
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-01-16 21:33:19 +0100

    🚨 java:S1128 Remove this unused import 'java.time.ZoneOffset'.

--- src/main/java/hu/bmiklos/bc/controller/mapper/DateTimeMapper.java
+++ src/main/java/hu/bmiklos/bc/controller/mapper/DateTimeMapper.java
@@ -3,7 +3,6 @@ package hu.bmiklos.bc.controller.mapper;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 
