c73355cc3d330883c16dec76309afd84d6bc2d5e
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-02-10 16:28:15 +0100

    🚨 java:S4449 Annotate the parameter with @javax.annotation.Nullable in constructor declaration, or make sure that null can not be passed as argument.

--- src/main/java/hu/bmiklos/bc/service/mapper/SuggestionDtoToSuggestionFormDataConverter.java
+++ src/main/java/hu/bmiklos/bc/service/mapper/SuggestionDtoToSuggestionFormDataConverter.java
@@ -3,6 +3,7 @@ package hu.bmiklos.bc.service.mapper;
 import java.util.Optional;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 import hu.bmiklos.bc.controller.dto.SuggestionFormData;
 import hu.bmiklos.bc.service.dto.BookAndSuggesterDto;
@@ -11,6 +12,7 @@ import hu.bmiklos.bc.service.dto.SuggestionDto;
 public class SuggestionDtoToSuggestionFormDataConverter implements Converter<BookAndSuggesterDto, SuggestionFormData> {
 
     @Override
+    @NonNull
     public SuggestionFormData convert(BookAndSuggesterDto source) {
         String bookId = source.getBook().getId().toString();
         String suggestionId = getSuggester(source)
