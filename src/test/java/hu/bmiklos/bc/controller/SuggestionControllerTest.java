a1c8bf2219830500eb5dd19ff594d7ae1d281e01
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-02-10 17:23:07 +0100

    ✅ edit/show functionality tested

---
+++ src/test/java/hu/bmiklos/bc/controller/SuggestionControllerTest.java
@@ -0,0 +1,63 @@
+package hu.bmiklos.bc.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import hu.bmiklos.bc.service.ActiveUserService;
+import hu.bmiklos.bc.service.SuggestionService;
+import hu.bmiklos.bc.service.dto.BookAndSuggesterDto;
+import hu.bmiklos.bc.service.dto.SuggestionDto;
+import hu.bmiklos.bc.service.dto.UserDto;
+
+class SuggestionControllerTest {
+    private ActiveUserService activeUserService;
+    private SuggestionController controller;
+    private SuggestionService suggestionService;
+
+    @BeforeEach
+    void setup() {
+        activeUserService = mock(ActiveUserService.class);
+        suggestionService = mock(SuggestionService.class);
+        controller = new SuggestionController(activeUserService, suggestionService);
+    }
+
+    @Test
+    void getSuggestionEditsOwnSuggestions() {
+        var testSuggestionId = UUID.randomUUID();
+        var testUserId = UUID.randomUUID();
+        var suggester = new UserDto(testUserId, "User", -1);
+        var suggestion = new SuggestionDto(testSuggestionId, Instant.now(), suggester, "Description");
+        var book = new BookAndSuggesterDto(UUID.randomUUID(), "Author", "Title", "url://", Set.of(suggestion));
+        when(suggestionService.getBookBySuggestionId(testSuggestionId)).thenReturn(book);
+        when(activeUserService.isCurrentUser(testUserId)).thenReturn(true);
+
+        ModelAndView result = controller.getSuggestion(testSuggestionId);
+
+        assertEquals("suggestion/edit", result.getViewName(), "The users should be able to edit their own suggestions.");
+    }
+
+    @Test
+    void getSuggestionShowsSuggestionsOfOthers() {
+        var testSuggestionId = UUID.randomUUID();
+        var testUserId = UUID.randomUUID();
+        var suggester = new UserDto(testUserId, "User", -1);
+        var suggestion = new SuggestionDto(testSuggestionId, Instant.now(), suggester, "Description");
+        var book = new BookAndSuggesterDto(UUID.randomUUID(), "Author", "Title", "url://", Set.of(suggestion));
+        when(suggestionService.getBookBySuggestionId(testSuggestionId)).thenReturn(book);
+        when(activeUserService.isCurrentUser(testUserId)).thenReturn(false);
+
+        ModelAndView result = controller.getSuggestion(testSuggestionId);
+
+        assertEquals("suggestion/show", result.getViewName(), "The users should be able to edit their own suggestions.");
+    }
+}
+
