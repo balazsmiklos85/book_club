12f13984911feecb227e7c48b141af80f4971216
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-10-22 11:03:57 +0200

    🎨 code formatting.

--- src/main/java/hu/bmiklos/bc/controller/UserController.java
+++ src/main/java/hu/bmiklos/bc/controller/UserController.java
@@ -1,51 +1,54 @@
 package hu.bmiklos.bc.controller;
 
+import hu.bmiklos.bc.controller.mapper.UserMapper;
+import hu.bmiklos.bc.model.User;
+import hu.bmiklos.bc.service.ActiveUserService;
+import hu.bmiklos.bc.service.UserService;
+import hu.bmiklos.bc.service.dto.UserDto;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import hu.bmiklos.bc.controller.mapper.UserMapper;
-import hu.bmiklos.bc.model.User;
-import hu.bmiklos.bc.service.ActiveUserService;
-import hu.bmiklos.bc.service.UserService;
-import hu.bmiklos.bc.service.dto.UserDto;
-
 @Controller
 public class UserController {
-    private final ActiveUserService activeUserService;
-    private final UserService userService;
+  private final ActiveUserService activeUserService;
+  private final UserService userService;
 
-    public UserController(ActiveUserService activeUserService, UserService userService) {
-	    this.activeUserService = activeUserService;
-	    this.userService = userService;
-    }
+  public UserController(ActiveUserService activeUserService, UserService userService) {
+    this.activeUserService = activeUserService;
+    this.userService = userService;
+  }
 
-    @GetMapping("/registration")
-    public ModelAndView registrationForm() {
-        return new ModelAndView("registration");
-    }
+  @GetMapping("/registration")
+  public ModelAndView registrationForm() {
+    return new ModelAndView("registration");
+  }
 
-    @PostMapping("/registration")
-    // TODO Too many parameters. Create a DTO class for this.
-    // I think this method was declared like this, because the DTO did not pick up the parameters with snake case names. Try to not use snake case names in the template.
-    public ModelAndView registration(@RequestParam("external_id") String externalId, @RequestParam String name, @RequestParam String email, @RequestParam("confirm_email") String confirmEmail, @RequestParam String password, @RequestParam("confirm_password") String confirmPassword) {
-        userService.registerUser(externalId, name, email, confirmEmail, password, confirmPassword);
-        return new ModelAndView("redirect:/login");
-    }
+  @PostMapping("/registration")
+  // TODO Too many parameters. Create a DTO class for this.
+  // I think this method was declared like this, because the DTO did not pick up the parameters with
+  // snake case names. Try to not use snake case names in the template.
+  public ModelAndView registration(
+      @RequestParam("external_id") String externalId,
+      @RequestParam String name,
+      @RequestParam String email,
+      @RequestParam("confirm_email") String confirmEmail,
+      @RequestParam String password,
+      @RequestParam("confirm_password") String confirmPassword) {
+    userService.registerUser(externalId, name, email, confirmEmail, password, confirmPassword);
+    return new ModelAndView("redirect:/login");
+  }
 
-    @GetMapping("/users")
-    public ModelAndView users() {
-	List<User> users = userService.findAll();
-	List<UserDto> usersResponse = UserMapper.mapToUserDtos(users);
-        ModelAndView modelAndView = new ModelAndView("user/list");
-        modelAndView.addObject("users", usersResponse);
-        modelAndView.addObject("isAdmin", activeUserService.isAdmin());
-        return modelAndView;
-
-    }
+  @GetMapping("/users")
+  public ModelAndView users() {
+    List<User> users = userService.findAll();
+    List<UserDto> usersResponse = UserMapper.mapToUserDtos(users);
+    ModelAndView modelAndView = new ModelAndView("user/list");
+    modelAndView.addObject("users", usersResponse);
+    modelAndView.addObject("isAdmin", activeUserService.isAdmin());
+    return modelAndView;
+  }
 }
--- src/main/java/hu/bmiklos/bc/service/UserService.java
+++ src/main/java/hu/bmiklos/bc/service/UserService.java
@@ -1,17 +1,10 @@
 package hu.bmiklos.bc.service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
-import hu.bmiklos.bc.exception.EmailAddressNotFoundException;
 import hu.bmiklos.bc.exception.NotAuthenticatedException;
 import hu.bmiklos.bc.exception.UserRegistrationException;
-import hu.bmiklos.bc.model.User;
 import hu.bmiklos.bc.model.Email;
 import hu.bmiklos.bc.model.Password;
+import hu.bmiklos.bc.model.User;
 import hu.bmiklos.bc.repository.EmailRepository;
 import hu.bmiklos.bc.repository.PasswordRepository;
 import hu.bmiklos.bc.repository.UserRepository;
@@ -20,122 +13,138 @@ import hu.bmiklos.bc.service.mapper.UserMapper;
 import hu.bmiklos.bc.service.security.SaltedPasswordEncoder;
 import hu.bmiklos.bc.util.SaltGenerator;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private final ActiveUserService activeUserService;
-    private final EmailRepository emailRepository;
-    private final UserRepository userRepository;
-    private final PasswordRepository passwordRepository;
+  private final ActiveUserService activeUserService;
+  private final EmailRepository emailRepository;
+  private final UserRepository userRepository;
+  private final PasswordRepository passwordRepository;
 
-    public UserService(ActiveUserService activeUserService, UserRepository userRepository, PasswordRepository passwordRepository, EmailRepository emailRepository) {
-        this.activeUserService = activeUserService;
-        this.userRepository = userRepository;
-        this.passwordRepository = passwordRepository;
-        this.emailRepository = emailRepository;
-    }
+  public UserService(
+      ActiveUserService activeUserService,
+      UserRepository userRepository,
+      PasswordRepository passwordRepository,
+      EmailRepository emailRepository) {
+    this.activeUserService = activeUserService;
+    this.userRepository = userRepository;
+    this.passwordRepository = passwordRepository;
+    this.emailRepository = emailRepository;
+  }
 
-    @Transactional
-    public String addEmail(String email) {
-        User user = userRepository.findById(activeUserService.getUserId())
+  @Transactional
+  public String addEmail(String email) {
+    User user =
+        userRepository
+            .findById(activeUserService.getUserId())
             .orElseThrow(NotAuthenticatedException::new);
-        Email createdEmail = createEmail(user, email, email);
-        return createdEmail.getEmailAddress();
-    }
+    Email createdEmail = createEmail(user, email, email);
+    return createdEmail.getEmailAddress();
+  }
 
-    @Transactional
-    public void changePassword(String oldPassword, String password, String confirmPassword) {
-        if (!password.equals(confirmPassword)) {
-            throw new IllegalArgumentException("The passwords do not match.");
-        }
-        User user = userRepository.findById(activeUserService.getUserId())
+  @Transactional
+  public void changePassword(String oldPassword, String password, String confirmPassword) {
+    if (!password.equals(confirmPassword)) {
+      throw new IllegalArgumentException("The passwords do not match.");
+    }
+    User user =
+        userRepository
+            .findById(activeUserService.getUserId())
             .orElseThrow(NotAuthenticatedException::new);
-        Password storedPassword = user.getPassword();
-        boolean authenticationSuccessful = new SaltedPasswordEncoder(storedPassword.getHashAlgorithm(), storedPassword.getSalt())
+    Password storedPassword = user.getPassword();
+    boolean authenticationSuccessful =
+        new SaltedPasswordEncoder(storedPassword.getHashAlgorithm(), storedPassword.getSalt())
             .matches(oldPassword, storedPassword.getPasswordHash());
-        if (!authenticationSuccessful) {
-            throw new IllegalArgumentException("The old password is incorrect.");
-        }
-        passwordRepository.delete(storedPassword);
-        passwordRepository.flush();
-        createPassword(user, password);
+    if (!authenticationSuccessful) {
+      throw new IllegalArgumentException("The old password is incorrect.");
+    }
+    passwordRepository.delete(storedPassword);
+    passwordRepository.flush();
+    createPassword(user, password);
+  }
+
+  @Transactional
+  public void deleteEmail(String email) {
+    Integer userEmailCount = emailRepository.countByUserId(activeUserService.getUserId());
+    if (userEmailCount <= 1) {
+      throw new IllegalArgumentException("You cannot delete your last email address.");
+    }
+    emailRepository.deleteById(email);
+  }
+
+  @Transactional
+  public void deleteUser(Email email) {
+    userRepository.delete(email.getUser());
+  }
+
+  public List<UserDto> getUsers() {
+    return userRepository.findAll().stream().map(UserMapper::mapToDto).toList();
+  }
+
+  public List<User> findAll() {
+    return userRepository.findAll();
+  }
+
+  @Transactional
+  public Email registerUser(
+      String externalId,
+      String name,
+      String email,
+      String confirmEmail,
+      String password,
+      String confirmPassword) {
+    if (!password.equals(confirmPassword)) {
+      throw new UserRegistrationException("The passwords do not match.");
     }
 
-    @Transactional
-    public void deleteEmail(String email) {
-        Integer userEmailCount = emailRepository.countByUserId(activeUserService.getUserId());
-        if (userEmailCount <= 1) {
-            throw new IllegalArgumentException("You cannot delete your last email address.");
-        }
-        emailRepository.deleteById(email);
+    try {
+      User user = createUser(externalId, name);
+      createPassword(user, password);
+      return createEmail(user, email, confirmEmail);
+    } catch (NumberFormatException e) {
+      throw new UserRegistrationException("The external ID must be a number.");
     }
+  }
 
-    @Transactional
-    public void deleteUser(Email email) {
-        userRepository.delete(email.getUser());
-    }
-
-    public List<UserDto> getUsers() {
-        return userRepository.findAll().stream()
-            .map(UserMapper::mapToDto)
-            .toList();
-    }
-
-    public List<User> findAll() {
-	return userRepository.findAll();	
-    }
-
-
-    @Transactional
-    public Email registerUser(String externalId, String name, String email, String confirmEmail, String password, String confirmPassword) {
-        if (!password.equals(confirmPassword)) {
-            throw new UserRegistrationException("The passwords do not match.");
-        }
-
-        try {
-            User user = createUser(externalId, name);
-            createPassword(user, password);
-            return createEmail(user, email, confirmEmail);
-        } catch (NumberFormatException e) {
-            throw new UserRegistrationException("The external ID must be a number.");
-        }
-    }
-
-    @Transactional
-    public void setAsAdmin(UUID id) {
-        var user = userRepository.findById(id)
+  @Transactional
+  public void setAsAdmin(UUID id) {
+    var user =
+        userRepository
+            .findById(id)
             .orElseThrow(() -> new IllegalArgumentException("User not found."));
-        user.setAdmin(true);
-        userRepository.saveAndFlush(user);
+    user.setAdmin(true);
+    userRepository.saveAndFlush(user);
+  }
+
+  private Email createEmail(User user, String emailAddress, String confirmEmail) {
+    if (!emailAddress.equals(confirmEmail)) {
+      throw new UserRegistrationException("The email addresses do not match.");
     }
 
-    private Email createEmail(User user, String emailAddress, String confirmEmail) {
-        if (!emailAddress.equals(confirmEmail)) {
-            throw new UserRegistrationException("The email addresses do not match.");
-        }
+    if (emailRepository.findById(emailAddress).isPresent()) {
+      throw new UserRegistrationException("This email address is already registered.");
+    }
+    var email = new Email(emailAddress, user);
+    return emailRepository.save(email);
+  }
 
-        if (emailRepository.findById(emailAddress).isPresent()) {
-            throw new UserRegistrationException("This email address is already registered.");
-        }
-        var email = new Email(emailAddress, user);
-        return emailRepository.save(email);
-    }
- 
-    private void createPassword(User user, String password) {
-        var salt = new SaltGenerator().generateSalt();
-        var passwordHash = new SaltedPasswordEncoder("bcrypt", salt)
-                .encode(password);
-        var userPassword = new Password(user, passwordHash, salt, "bcrypt");
-        passwordRepository.save(userPassword);
-    }
+  private void createPassword(User user, String password) {
+    var salt = new SaltGenerator().generateSalt();
+    var passwordHash = new SaltedPasswordEncoder("bcrypt", salt).encode(password);
+    var userPassword = new Password(user, passwordHash, salt, "bcrypt");
+    passwordRepository.save(userPassword);
+  }
 
-    private User createUser(String externalId, String name) {
-        var extId = Integer.parseInt(externalId);
-        if (userRepository.findByExternalId(extId).isPresent()) {
-            throw new UserRegistrationException("This external ID is already registered.");
-        }
-        var user = new User(name, false, extId);
-        return userRepository.save(user);
+  private User createUser(String externalId, String name) {
+    var extId = Integer.parseInt(externalId);
+    if (userRepository.findByExternalId(extId).isPresent()) {
+      throw new UserRegistrationException("This external ID is already registered.");
     }
+    var user = new User(name, false, extId);
+    return userRepository.save(user);
+  }
 }
