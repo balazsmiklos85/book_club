b89103842ff6a57d7375d1ffac5440c61d8208e9
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-03-16 09:19:09 +0100

    ✨ New, yet unimplemented suggestion deletion endpoint.

--- adapter-web-thymeleaf/src/main/java/hu/bmiklos/bc/web/controller/SuggestionController.java
+++ adapter-web-thymeleaf/src/main/java/hu/bmiklos/bc/web/controller/SuggestionController.java
@@ -1,8 +1,17 @@
 package hu.bmiklos.bc.web.controller;
 
+import java.util.UUID;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/suggestion")
 public class SuggestionController {
-
+  @PostMapping("/{id}/deletion")
+  public String deleteSuggestion(@PathVariable UUID id) {
+    // TODO implement
+    throw new UnsupportedOperationException();
+  }
 }
