e49a841d84decc6ca0ed7e74ef1042df26ad4a1d
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-09-30 11:00:42 +0200

    🚨 java:S2479 Whitespace and control characters in literals should be explicit
    Remove the non-escaped \u0009 character from this literal.

--- adapter-jpa/src/main/java/hu/bmiklos/bc/model/repository/BookJpaRepository.java
+++ adapter-jpa/src/main/java/hu/bmiklos/bc/model/repository/BookJpaRepository.java
@@ -15,24 +15,24 @@ public interface BookJpaRepository extends JpaRepository<BookEntity, UUID> {
           """
             select book_id, sum(user_weight)
             from (select p.participant_external_id as user_external_id, vote_union.book_id, count(p.participant_external_id) as user_weight
-            	  from participants p
-            	  inner join (select v.user_external_id , v.book_id
-            	              from votes v
-            	              where v.user_external_id is not null
-            	              union
-            	              select u.external_id , v.book_id
-            	              from votes v
-            	              inner join users u
-            	              on u.id  = v.user_id) as vote_union
-            	    on vote_union.user_external_id = p.participant_external_id
-            	  where p.event_id in (select id
-            	                       from (select distinct e.id, e.time
-            	                             from events e
-            	                             inner join participants p
-            	                               on p.event_id = e.id
-            	                             order by e.time desc
-            	                             limit 12))
-            	  group by p.participant_external_id, vote_union.book_id)
+                  from participants p
+                  inner join (select v.user_external_id , v.book_id
+                              from votes v
+                              where v.user_external_id is not null
+                              union
+                              select u.external_id , v.book_id
+                              from votes v
+                              inner join users u
+                              on u.id = v.user_id) as vote_union
+                    on vote_union.user_external_id = p.participant_external_id
+                  where p.event_id in (select id
+                                       from (select distinct e.id, e.time
+                                             from events e
+                                             inner join participants p
+                                               on p.event_id = e.id
+                                             order by e.time desc
+                                             limit 12))
+                  group by p.participant_external_id, vote_union.book_id)
             group by book_id
             order by sum(user_weight) desc
             """,
