ae4366679530ccf79275b0c474b738e7b4302b17
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-10-16 10:41:19 +0200

    🐛 '\'' is a special character for `MessageFormat`.

--- business/src/main/java/hu/bmiklos/bc/business/exception/EntityNotFoundException.java
+++ business/src/main/java/hu/bmiklos/bc/business/exception/EntityNotFoundException.java
@@ -4,6 +4,6 @@ import java.text.MessageFormat;
 
 public class EntityNotFoundException extends RuntimeException {
   public EntityNotFoundException(Object id) {
-    super(MessageFormat.format("Entity with id '{0}' not found.", id));
+    super(MessageFormat.format("Entity with id ''{0}'' not found.", id));
   }
 }
