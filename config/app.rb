c9ef9feca2d764f404ea079540ab4de580ba9e06
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-03-11 19:44:11 +0100

    ✨ enabling sessions using cookies.

--- config/app.rb
+++ config/app.rb
@@ -3,6 +3,11 @@
 require 'hanami'
 
 module BookClub
+  # Main class of the book club application.
   class App < Hanami::App
+    config.actions.sessions = :cookie, {
+      key: 'book_club.session',
+      secret: ENV.fetch('SESSION_SECRET')
+    }
   end
 end
