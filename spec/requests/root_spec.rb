b8771bc45bfbfc056f7acc1c973b2b6a583275cf
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-04-01 21:44:17 +0200

    ✅ root_spec fixed

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Root', type: :request do
-  it 'is not found' do
+  it 'routes to leaderboard index' do
     get '/'
 
-    # Generate new action via:
-    #   `bundle exec hanami generate action home.index --url=/`
-    expect(last_response.status).to be(404)
+    expect(last_response.status).to eq(200)
   end
 end
