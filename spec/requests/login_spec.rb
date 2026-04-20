8e400f04ccf328e364d82764da2b5a5ac33b7753
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-04-20 20:33:36 +0200

    🧪 adding a missing test for the login form.

---
+++ spec/requests/login_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Login', type: :request do
+  describe 'GET /login' do
+    it 'returns 200 status' do
+      get '/login'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'displays the login form' do
+      get '/login'
+      expect(last_response.body).to include('login')
+    end
+
+    it 'renders the login form template' do
+      get '/login'
+      expect(last_response.body).to include('Email')
+      expect(last_response.body).to include('Password')
+    end
+  end
+end
