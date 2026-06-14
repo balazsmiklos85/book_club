08af3e65d1c25daf55ea6fff3e0a52bedd7435a2
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-06-14 10:13:46 +0200

    🐛 Fix the register operation to pass `user_id` to `create_password` instead of `external_id`

--- app/operations/register.rb
+++ app/operations/register.rb
@@ -13,8 +13,8 @@ module BookClub
       def call(name:, email:, password:, confirm_password:, external_id:)
         step validate_password_match(password, confirm_password)
         # TODO: email.downcase should be a User domain concern
-        step create_user(name: name, email: email.downcase, external_id: external_id)
-        step create_password(external_id, password)
+        user_id = step create_user(name: name, email: email.downcase, external_id: external_id)
+        step create_password(user_id, password)
         # TODO: Email is not created and persisted for the User
         external_id
       end
