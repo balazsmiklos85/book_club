fc490bd17146913f12139d8e4c1456ff81e57956
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-07-12 17:52:03 +0200

    🚨 Metrics/AbcSize & MethodLength: Refactored register create action

--- app/actions/register/create.rb
+++ app/actions/register/create.rb
@@ -8,22 +8,13 @@ module BookClub
         include Deps['operations.register']
 
         def handle(request, response)
-          result = register.call(
-            name: request.params[:name],
-            email: request.params[:email],
-            confirm_email: request.params[:confirm_email],
-            password: request.params[:password],
-            confirm_password: request.params[:confirm_password],
-            external_id: request.params[:external_id].to_i
-          )
-
-          case result
-          when Success
+          result = register.call request.params.to_h
+          if result.success?
             response.session[:external_id] = result.value!
-            response.redirect_to(root_path, status: :see_other)
-          when Failure
-            response.flash[:error] = I18n.t('book_club.flash.registration_failed')
-            response.redirect_to(register_path, status: :see_other)
+            response.redirect_to root_path, status: :see_other
+          else
+            response.flash[:error] = I18n.t 'book_club.flash.registration_failed'
+            response.redirect_to register_path, status: :see_other
           end
         end
 
